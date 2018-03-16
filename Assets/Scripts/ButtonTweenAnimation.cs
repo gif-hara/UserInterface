59ffe52da9cf7f014e68ded23f6d81f38e0ff09f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-17 01:42:45 +0900

    :new: とりあえずボタン置く

---
+++ Assets/Scripts/ButtonTweenAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.UserInterface
+{
+    /// <summary>
+    /// ボタンのアニメーションを
+    /// </summary>
+    public class ButtonTweenAnimation : MonoBehaviour
+    {
+    }
+}
