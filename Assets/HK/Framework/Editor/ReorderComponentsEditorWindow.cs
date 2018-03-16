aaa284ef4fceb0af4686629bbbf310d122541474
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-17 01:55:12 +0900

    :new: HK.Framework

---
+++ Assets/HK/Framework/Editor/ReorderComponentsEditorWindow.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEditor.SceneManagement;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+namespace HK.Framework
+{
+	/// <summary>
+	/// コンポーネントのMove UpとMove Downを容易に行うEditorWindow.
+	/// </summary>
+	public class ReorderComponentsEditorWindow : EditorWindow
+	{
+		private ReorderableList componentList;
+
+		private GameObject selectionObject;
+
+		private Component selectionComponent;
+
+		private int selectionIndex;
+
+		[MenuItem("Window/ReorderComponents")]
+		public static void ShowWindow()
+		{
+			EditorWindow.GetWindow<ReorderComponentsEditorWindow>();
+		}
+
+		void OnGUI()
+		{
+			if(this.selectionObject == null)
+			{
+				EditorGUILayout.LabelField("Please select a GameObject.");
+			}
+			else
+			{
+				EditorGUILayout.ObjectField(this.selectionObject, typeof(GameObject), true);
+			}
+			if(this.componentList != null)
+			{
+				this.componentList.DoLayoutList();
+			}
+		}
+
+		void OnInspectorUpdate()
+		{
+			if(this.selectionObject != Selection.activeGameObject)
+			{
+				this.selectionObject = Selection.activeGameObject;
+				this.CreateComponentList();
+				this.Repaint();
+			}
+		}
+
+		private void CreateComponentList()
+		{
+			if(this.selectionObject == null)
+			{
+				this.componentList = null;
+				return;
+			}
+				
+			var components = new List<Component>(this.selectionObject.GetComponents<Component>());
+			components.RemoveAll(this.IsRemove);
+			this.componentList = new ReorderableList(components, typeof(Component));
+			this.componentList.drawElementCallback = ( Rect rect, int index, bool selected, bool focused ) =>
+			{
+				var property = this.componentList.list[index] as Component;
+				EditorGUI.LabelField(rect, new GUIContent( property.GetType().ToString() ) );
+			};
+			this.componentList.drawHeaderCallback = ( Rect rect ) =>
+			{
+				EditorGUI.LabelField(rect, "Components");
+			};
+			this.componentList.drawFooterCallback = (Rect rect) =>
+			{
+			};
+			this.componentList.onReorderCallback = (ReorderableList list) => 
+			{
+				float sign = Mathf.Sign(list.index - this.selectionIndex);
+				int difference = Mathf.Abs(list.index - this.selectionIndex);
+				for(int i=0; i<difference; i++)
+				{
+					if(sign < 0)
+					{
+						UnityEditorInternal.ComponentUtility.MoveComponentUp(this.selectionComponent);
+					}
+					else
+					{
+						UnityEditorInternal.ComponentUtility.MoveComponentDown(this.selectionComponent);
+					}
+				}
+				if(difference != 0)
+				{
+					EditorUtility.SetDirty(this.selectionObject);
+					EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+				}
+			};
+			this.componentList.onSelectCallback = (ReorderableList list) => 
+			{
+				this.selectionIndex = list.index;
+				this.selectionComponent = list.list[list.index] as Component;
+			};
+		}
+
+		private bool IsRemove(Component target)
+		{
+			return target.GetType() == typeof(UnityEngine.Transform);
+		}
+	}
+}
---
+++ Assets/HK/Framework/Scripts/EventSystems/UniRxEvent.cs
@@ -0,0 +1,187 @@
+﻿using UniRx;
+
+namespace HK.Framework.EventSystems
+{
+    /// <summary>
+    /// UniRxイベントの基底クラス.
+    /// </summary>
+    public abstract class UniRxEvent
+    {
+        public static IMessageBroker GlobalBroker { get { return MessageBroker.Default; } }
+    }
+
+    public abstract class UniRxEvent<E> : UniRxEvent
+        where E : UniRxEvent<E>, new()
+    {
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
+        public static E Get()
+        {
+            var result = new E();
+
+            return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache()
+        {
+            return cache;
+        }
+    }
+
+    public abstract class UniRxEvent<E, P1> : UniRxEvent
+        where E : UniRxEvent<E, P1>, new()
+    {
+        protected P1 param1;
+
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
+        public static E Get(P1 param1)
+        {
+            var result = new E();
+            result.param1 = param1;
+
+            return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1)
+        {
+            cache.param1 = param1;
+            return cache;
+        }
+    }
+
+    public abstract class UniRxEvent<E, P1, P2> : UniRxEvent
+        where E : UniRxEvent<E, P1, P2>, new()
+    {
+        protected P1 param1;
+
+        protected P2 param2;
+
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
+        public static E Get(P1 param1, P2 param2)
+        {
+            var result = new E();
+            result.param1 = param1;
+            result.param2 = param2;
+
+            return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1, P2 param2)
+        {
+            cache.param1 = param1;
+            cache.param2 = param2;
+            return cache;
+        }
+    }
+
+    public abstract class UniRxEvent<E, P1, P2, P3> : UniRxEvent
+        where E : UniRxEvent<E, P1, P2, P3>, new()
+    {
+        protected P1 param1;
+
+        protected P2 param2;
+
+        protected P3 param3;
+
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
+        public static E Get(P1 param1, P2 param2, P3 param3)
+        {
+            var result = new E();
+            result.param1 = param1;
+            result.param2 = param2;
+            result.param3 = param3;
+
+            return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1, P2 param2, P3 param3)
+        {
+            cache.param1 = param1;
+            cache.param2 = param2;
+            cache.param3 = param3;
+            return cache;
+        }
+    }
+
+    public abstract class UniRxEvent<E, P1, P2, P3, P4> : UniRxEvent
+        where E : UniRxEvent<E, P1, P2, P3, P4>, new()
+    {
+        protected P1 param1;
+
+        protected P2 param2;
+
+        protected P3 param3;
+
+        protected P4 param4;
+
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
+        public static E Get(P1 param1, P2 param2, P3 param3, P4 param4)
+        {
+            var result = new E();
+            result.param1 = param1;
+            result.param2 = param2;
+            result.param3 = param3;
+            result.param4 = param4;
+
+            return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1, P2 param2, P3 param3, P4 param4)
+        {
+            cache.param1 = param1;
+            cache.param2 = param2;
+            cache.param3 = param3;
+            cache.param4 = param4;
+            return cache;
+        }
+    }
+}
