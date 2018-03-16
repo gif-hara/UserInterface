df30ef3b3544bef85c2057e5d18fab03a74eba61
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-17 03:25:40 +0900

    :new: アニメーション出来るように

--- Assets/HK/UserInterface/Scripts/ButtonTweenAnimation.cs
+++ Assets/HK/UserInterface/Scripts/ButtonTweenAnimation.cs
@@ -1,5 +1,5 @@
 ﻿using DG.Tweening;
-using UniRx.Triggers;
+using HK.UserInterface.Animations;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.EventSystems;
@@ -9,10 +9,70 @@ namespace HK.UserInterface
     /// <summary>
     /// ボタンのアニメーションを<see cref="DOTween"/>で行うクラス
     /// </summary>
-    public class ButtonTweenAnimation : MonoBehaviour, IPointerDownHandler
+    public class ButtonTweenAnimation :
+        MonoBehaviour,
+        IPointerDownHandler,
+        IPointerUpHandler,
+        IPointerClickHandler,
+        IPointerEnterHandler,
+        IPointerExitHandler
     {
+        [SerializeField]
+        private SequenceObject pointerDown;
+
+        [SerializeField]
+        private SequenceObject pointerUp;
+
+        private GameObject cachedGameObject;
+
+        private Sequence currentSequence;
+
+        private bool pressed;
+
+        void Awake()
+        {
+            Assert.IsNotNull(this.pointerDown);
+            Assert.IsNotNull(this.pointerUp);
+            this.cachedGameObject = this.gameObject;
+        }
+        
         public void OnPointerDown(PointerEventData eventData)
         {
+            this.InvokeSequence(this.pointerDown.Invoke(this.cachedGameObject));
+            this.pressed = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            this.pressed = false;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            this.InvokeSequence(this.pointerUp.Invoke(this.cachedGameObject));
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (this.pressed)
+            {
+                this.InvokeSequence(this.pointerDown.Invoke(this.cachedGameObject));
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            this.InvokeSequence(this.pointerUp.Invoke(this.cachedGameObject));
+        }
+
+        private void InvokeSequence(Sequence sequence)
+        {
+            if (this.currentSequence != null)
+            {
+                this.currentSequence.Kill();
+            }
+
+            this.currentSequence = sequence;
         }
     }
 }
