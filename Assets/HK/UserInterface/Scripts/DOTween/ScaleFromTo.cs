6b155b5cda51838d669c1ea2354c5ff9f8b98ca7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-17 23:32:02 +0900

    :new: ScaleFromTo追加

---
+++ Assets/HK/UserInterface/Scripts/DOTween/ScaleFromTo.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace HK.UserInterface.Animations
+{
+    /// <summary>
+    /// スケールアニメーションを行う
+    /// </summary>
+    [CreateAssetMenu(menuName = "HK/UserInterface/Animations/Tween/ScaleFromTo")]
+    public sealed class ScaleFromTo : TweenObject
+    {
+        [SerializeField]
+        private Vector3 from;
+        
+        [SerializeField]
+        private Vector3 to;
+        
+        public override Tween Tween(TweenTarget target)
+        {
+            target.Graphic.transform.localScale = this.from;
+            return target.Graphic.transform.DOScale(this.to, this.duration).SetEase(this.ease);
+        }
+
+        #if UNITY_EDITOR
+        protected override string AssetName { get { return string.Format("{0}_{1}_{2}", base.AssetName, this.from, this.to); } }
+        #endif
+    }
+}
