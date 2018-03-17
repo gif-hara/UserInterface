6738ed012a35baeeb5b7ab58d0d334ac0766524d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-17 13:32:19 +0900

    :new: カラーアニメーション出来るようにしたのとテクスチャ追加

---
+++ Assets/HK/UserInterface/Scripts/DOTween/ColorTo.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HK.UserInterface.Animations
+{
+    /// <summary>
+    /// スケールアニメーションを行う
+    /// </summary>
+    [CreateAssetMenu(menuName = "HK/UserInterface/Animations/Tween/ColorTo")]
+    public sealed class ColorTo : TweenObject
+    {
+        [SerializeField]
+        private Color to;
+        
+        public override Tween Tween(GameObject gameObject)
+        {
+            return gameObject.GetComponent<Graphic>().DOColor(this.to, this.duration).SetEase(this.ease);
+        }
+
+        #if UNITY_EDITOR
+        protected override string AssetName { get { return string.Format("ColorTo_{0}_{1}", base.AssetName, this.to); } }
+        #endif
+    }
+}
