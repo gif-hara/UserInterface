81978dec65246b519255f57c5516c2ea9c7f5b32
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-17 13:59:22 +0900

    :new: TweenTarget追加

---
+++ Assets/HK/UserInterface/Scripts/DOTween/TweenTarget.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.UserInterface.Animations
+{
+    /// <summary>
+    /// アニメーションするオブジェクトを制御するクラス
+    /// </summary>
+    public sealed class TweenTarget : MonoBehaviour
+    {
+        [SerializeField]
+        private Graphic target;
+
+        private Color defaultColor;
+
+        public Graphic Target { get { return target; } set { target = value; } }
+
+        void Awake()
+        {
+            if (this.target == null)
+            {
+                this.target = this.GetComponent<Graphic>();
+            }
+            
+            Assert.IsNotNull(this.target);
+            this.defaultColor = this.target.color;
+            Debug.Log(this.defaultColor);
+        }
+        
+        #if UNITY_EDITOR
+        void Reset()
+        {
+            this.target = this.GetComponent<Graphic>();
+        }
+        #endif
+    }
+}
