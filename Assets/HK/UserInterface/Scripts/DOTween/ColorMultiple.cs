7d7d0bc5932be43eb2bd3ff6858c531185d1e3fe
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-17 22:12:40 +0900

    :new: ColorMultiple追加

---
+++ Assets/HK/UserInterface/Scripts/DOTween/ColorMultiple.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace HK.UserInterface.Animations
+{
+    /// <summary>
+    /// 乗算カラーアニメーションを行う
+    /// </summary>
+    [CreateAssetMenu(menuName = "HK/UserInterface/Animations/Tween/ColorMultiple")]
+    public sealed class ColorMultiple : TweenObject
+    {
+        [SerializeField]
+        private Color to;
+
+        [SerializeField]
+        private float multiple = 1.0f;
+        
+        public override Tween Tween(TweenTarget target)
+        {
+            var multipleTo = target.DefaultColor * (this.to * this.multiple);
+            return target.Graphic.DOColor(multipleTo, this.duration).SetEase(this.ease);
+        }
+
+        #if UNITY_EDITOR
+        protected override string AssetName { get { return string.Format("{0}_{1}_{2}", base.AssetName, this.to, this.multiple); } }
+        #endif
+    }
+}
