a851d889cefbebae80a80dd733352600e43e6a32
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-18 00:06:34 +0900

    :new: Panelに親子関係を作る

--- Assets/HK/UserInterface/Scripts/SceneManagements/PanelController.cs
+++ Assets/HK/UserInterface/Scripts/SceneManagements/PanelController.cs
@@ -1,4 +1,5 @@
-﻿using HK.UserInterface.SceneManagements;
+﻿using System.Collections.Generic;
+using HK.UserInterface.SceneManagements;
 using UniRx;
 using UnityEngine;
 
@@ -9,6 +10,25 @@ namespace HK.UserInterface.SceneManagements
     /// </summary>
     public abstract class PanelController : MonoBehaviour, IPanel
     {
+        protected PanelController parent;
+
+        protected List<PanelController> children = new List<PanelController>();
+
+        public void SetParent(PanelController parent)
+        {
+            if (this.parent != null)
+            {
+                this.parent.children.Remove(this);
+            }
+
+            this.parent = parent;
+            
+            if (this.parent != null)
+            {
+                this.parent.children.Add(this);
+            }
+        }
+        
         public abstract IObservable<Unit> OnPanelIn();
 
         public abstract IObservable<Unit> OnPanelOut();
