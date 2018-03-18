9b36e157bdace997f8b18a7e348bd09a00e984c2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-19 04:16:35 +0900

    :new: ColorType追加

---
+++ Assets/HK/UserInterface/Scripts/ColorType.cs
@@ -0,0 +1,29 @@
+﻿namespace HK.UserInterface.Enums
+{
+    /// <summary>
+    /// https://flatuicolors.com/palette/us
+    /// </summary>
+    public enum ColorType
+    {
+        LightGreenishBlue,
+        FadeoPoster,
+        GreenDarnerTail,
+        ShyMoment,
+        MintLeaf,
+        RobinsEggBlue,
+        ElectronBlue,
+        ExodusFruit,
+        SourLemon,
+        FirstDate,
+        PinkGlamour,
+        Pico8Pink,
+        BrightYarrow,
+        OrangeVille,
+        ChiGong,
+        PrunusAvium,
+        CityLight,
+        SoothingBreeze,
+        AmericanRiver,
+        DraculaOrchid,
+    }
+}
