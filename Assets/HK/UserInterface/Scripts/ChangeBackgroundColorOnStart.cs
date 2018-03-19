8ff739be1d0e3220d30d902eb451d3e72c550751
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-20 01:10:11 +0900

    :new: Startのタイミングで背景色を切り替えられるように

---
+++ Assets/HK/UserInterface/Scripts/ChangeBackgroundColorOnStart.cs
@@ -0,0 +1,24 @@
+﻿using HK.Framework.EventSystems;
+using HK.UserInterface.Enums;
+using HK.UserInterface.Events.GameSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.UserInterface.GameSystems
+{
+    /// <summary>
+    /// <see cref="Start"/>のタイミングで背景色を切り替える
+    /// </summary>
+    public sealed class ChangeBackgroundColorOnStart : MonoBehaviour
+    {
+        [SerializeField]
+        private ColorType colorType;
+
+        public ColorType ColorType { get { return colorType; } set { colorType = value; } }
+        
+        void Start()
+        {
+            UniRxEvent.GlobalBroker.Publish(ChangeBackgroundColor.GetCache(this.colorType));
+        }
+    }
+}
