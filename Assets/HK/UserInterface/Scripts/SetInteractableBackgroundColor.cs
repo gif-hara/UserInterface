156c225c8d026925fa3ce5bc00b3720237fdb308
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-03-24 15:20:15 +0900

    :new: 背景色からInteractableを設定できるように

---
+++ Assets/HK/UserInterface/Scripts/SetInteractableBackgroundColor.cs
@@ -0,0 +1,32 @@
+﻿using HK.UserInterface.Enums;
+using HK.UserInterface.GameSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.UserInterface
+{
+    /// <summary>
+    /// 現状の背景色から操作可能か判断する
+    /// </summary>
+    public sealed class SetInteractableBackgroundColor : MonoBehaviour
+    {
+        [SerializeField]
+        private Selectable selectable;
+
+        [SerializeField]
+        private ColorType negativeColorType;
+
+        void Start()
+        {
+            this.selectable.interactable = BackgroundColorController.Instance.CurrentColorType != this.negativeColorType;
+        }
+        
+        #if UNITY_EDITOR
+        void Reset()
+        {
+            this.selectable = this.GetComponent<Selectable>();
+        }
+        #endif
+    }
+}
